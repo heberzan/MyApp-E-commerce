c90df2a81f94d811545e16622073f475a96e0918
Heberzan <hebermarzan@gmail.com>
2025-05-24 14:23:50 -0500

    Homework II - FSM4FEL2-React & Typescript

--- back/src/config/dataSource.ts
+++ back/src/config/dataSource.ts
@@ -1,20 +1,20 @@
-import { DataSource } from "typeorm";
-import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from "./envs";
-import { User } from "../entities/User";
-import { Credential } from "../entities/Credential";
-import { Order } from "../entities/Order";
-import { Category } from "../entities/Category";
-import { Product } from "../entities/Product";
+import { DataSource } from 'typeorm';
+import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './envs';
+import { User } from '../entities/User';
+import { Credential } from '../entities/Credential';
+import { Order } from '../entities/Order';
+import { Category } from '../entities/Category';
+import { Product } from '../entities/Product';
 
 export const AppDataSource = new DataSource({
-  type: "postgres",
+  type: 'postgres',
   host: DB_HOST,
   port: DB_PORT,
   username: DB_USER,
   password: DB_PASSWORD,
   database: DB_NAME,
   synchronize: true,
-  // dropSchema: true,
+  // dropSchema: true, // Set to true only for development - // Para limpiar la base de datos (true).
   logging: false,
   entities: [User, Credential, Order, Product, Category],
   subscribers: [],
--- back/src/helpers/preLoadProducts.ts
+++ back/src/helpers/preLoadProducts.ts
@@ -1,6 +1,6 @@
-import { AppDataSource } from "../config/dataSource";
-import { Product } from "../entities/Product";
-import { ProductRepository } from "../repositories/product.repository";
+import { AppDataSource } from '../config/dataSource';
+import { Product } from '../entities/Product';
+import { ProductRepository } from '../repositories/product.repository';
 
 interface IProduct {
   name: string;
@@ -13,62 +13,62 @@ interface IProduct {
 
 const productsToPreLoad: IProduct[] = [
   {
-    name: "iPhone 11",
+    name: 'iPhone 16 Pro',
     price: 699,
     description:
-      "Experience power and elegance with the iPhone 11: capture stunning moments with its dual-camera system, enjoy exceptional performance, and immerse yourself in a brilliant Liquid Retina display. Discover a world of possibilities in the palm of your hand!",
+      'Experience power and elegance with the iPhone 11: capture stunning moments with its dual-camera system, enjoy exceptional performance, and immerse yourself in a brilliant Liquid Retina display. Discover a world of possibilities in the palm of your hand!',
     image:
-      "https://www.apple.com/v/iphone-11/a/images/meta/og__f2j3dwkzna2u.png",
+      'https://www.apple.com/newsroom/images/2024/09/apple-debuts-iphone-16-pro-and-iphone-16-pro-max/article/Apple-iPhone-16-Pro-hero-geo-240909_inline.jpg.large.jpg',
     categoryId: 1,
     stock: 10,
   },
   {
-    name: "MacBook Air",
+    name: 'MacBook Air',
     price: 999,
     description:
-      "Embrace efficiency and sophistication with the MacBook Air: lightweight design meets powerful performance, stunning Retina display brings your work to life, and all-day battery life keeps you productive wherever you go. Elevate your computing experience with the MacBook Air.",
+      'Embrace efficiency and sophistication with the MacBook Air: lightweight design meets powerful performance, stunning Retina display brings your work to life, and all-day battery life keeps you productive wherever you go. Elevate your computing experience with the MacBook Air.',
     image:
-      "https://www.apple.com/v/macbook-air/a/images/meta/og__d5k62k8b4qka.png",
+      'https://wallpapers.com/images/high/black-macbook-1200-x-800-32aunj6i8jioq0un.webp',
     categoryId: 2,
     stock: 10,
   },
   {
-    name: "iPad Pro",
+    name: 'iPad Pro',
     price: 799,
     description:
-      "Unleash your creativity and productivity with the iPad Pro: powerful performance, stunning Liquid Retina display, and all-day battery life make the iPad Pro the perfect tool for work and play. Transform your ideas into reality with the iPad Pro.",
+      'Unleash your creativity and productivity with the iPad Pro: powerful performance, stunning Liquid Retina display, and all-day battery life make the iPad Pro the perfect tool for work and play. Transform your ideas into reality with the iPad Pro.',
     image:
-      "https://www.apple.com/v/ipad-pro/a/images/meta/og__d8m6x7smkntm.png",
+      'https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2021/04/ipad-pro-2021-2317515.jpg?tf=1200x',
     categoryId: 3,
     stock: 10,
   },
   {
-    name: "Apple Watch Series 6",
+    name: 'Apple Watch Series 6',
     price: 399,
     description:
-      "Stay connected and healthy with the Apple Watch Series 6: track your workouts, monitor your health, and stay in touch with the people and information you care about most. Experience the future of health and wellness with the Apple Watch Series 6.",
+      'Stay connected and healthy with the Apple Watch Series 6: track your workouts, monitor your health, and stay in touch with the people and information you care about most. Experience the future of health and wellness with the Apple Watch Series 6.',
     image:
-      "https://www.apple.com/v/apple-watch-series-6/a/images/meta/og__c1zv8c8n7q06.png",
+      'https://www.apple.com/newsroom/images/product/watch/standard/Apple_delivers-apple-watch-series-6_09152020_big.jpg.large.jpg',
     categoryId: 4,
     stock: 10,
   },
   {
-    name: "AirPods Pro",
+    name: 'AirPods Pro',
     price: 249,
     description:
-      "Immerse yourself in sound with the AirPods Pro: active noise cancellation, transparency mode, and customizable fit make the AirPods Pro the perfect companion for music, calls, and everything in between. Elevate your audio experience with the AirPods Pro.",
+      'Immerse yourself in sound with the AirPods Pro: active noise cancellation, transparency mode, and customizable fit make the AirPods Pro the perfect companion for music, calls, and everything in between. Elevate your audio experience with the AirPods Pro.',
     image:
-      "https://www.apple.com/v/airpods-pro/a/images/meta/og__c1zv8c8n7q06.png",
+      'https://lamanzanamordida.net/app/uploads-lamanzanamordida.net/2019/11/AirPods-Pro-cancelacion-de-ruido-1024x576.jpg',
     categoryId: 5,
     stock: 10,
   },
   {
-    name: "HomePod mini",
+    name: 'HomePod mini',
     price: 99,
     description:
-      "Elevate your home audio experience with the HomePod mini: immersive sound, intelligent assistant, and smart home hub make the HomePod mini the perfect addition to your home. Enjoy a world of music, news, and more with the HomePod mini.",
+      'Elevate your home audio experience with the HomePod mini: immersive sound, intelligent assistant, and smart home hub make the HomePod mini the perfect addition to your home. Enjoy a world of music, news, and more with the HomePod mini.',
     image:
-      "https://www.apple.com/v/homepod-mini/a/images/meta/og__d5k62k8b4qka.png",
+      'https://media.istockphoto.com/id/1305312556/es/foto/apple-homepod-mini-sobre-un-fondo-oscuro.jpg?s=1024x1024&w=is&k=20&c=Hje5OreE0oOYhyphXiuudV-xrnp4zfJWR3vyZVhhzB8=',
     categoryId: 6,
     stock: 10,
   },
@@ -82,5 +82,5 @@ export const preLoadProducts = async () => {
       .into(Product)
       .values(productsToPreLoad)
       .execute();
-  console.log("Products preloaded");
+  console.log('Products preloaded');
 };
